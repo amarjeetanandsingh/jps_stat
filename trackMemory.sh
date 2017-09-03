dc5d6acbaaf661b78660ce63c04a128aea18b1ea
amarjeet <a.anand@everteam.com>
2017-09-03 09:27:13 +0530

    repaired erasing previous line and working with max=current
    * Currently working good with prev line deletes
    *
    BUGS :
    1. max = current
    2. if there is no jps, previous line delete keep deleting terminal in backward direction

--- trackMemory.sh
+++ trackMemory.sh
@@ -2,34 +2,32 @@
 
 MEMORY=0.0
 
-declare -A prev_pid_name=([1234]="Jps")
-declare -A prev_name_max=(["Jps"]=0)
-declare -A curr_pid_name=([1234]="Jps")
-declare -A curr_name_max=(["Jps"]=0)
+declare -A prev_name_max=()
+declare -A curr_pid_name=()
+declare -A curr_name_max=()
 
 echo "pid    Name                                          CurrMem Max "
 echo "====   ============================================  ======= ===="
+printf "\n"
 
 
 
 
 while true
-do
-    
-    
+do   
     IFS=$'\n'
     DATA=($("jps"))
-    #insert process data into array
-    IFS=$' '
-    
     
     #clear previous loop output from screen
-    tput cuu $(( ${#prev_pid_name[@]} -1 )) && tput el
+    tput cuu $(( ${#prev_name_max[@]} )) && tput el
     
+    #for each we get in jps in current loop
+    IFS=$' '
     for LINE in "${DATA[@]}"
     do
         read -ra TOKENS <<< "$LINE"
         
+        #skip the process if its Jps or Jstat itself 
         if [ "${TOKENS[1]}" == "Jps" ] || [ "${TOKENS[1]}" == "Jstat" ] || [ "${TOKENS[0]}" -eq 0 ]
         then
             continue
@@ -48,39 +46,29 @@ do
 
 
     #get the memroy use for each pid in curr_pid_name
-    
     for pid in "${!curr_pid_name[@]}"; 
     do
         name=${curr_pid_name["$pid"]}
-        if [ "$name" == "Jps" ] || [ "$name" == "Jstat" ] || [ "$pid" -eq 0 ]
+        MEMORY=$(jstat -gc $pid | tail -n 1 | awk '{split($0,a," "); sum=a[3]+a[4]+a[6]+a[8]; mb=sum/1024; print mb}')
+        if (( ${prev_name_max[$name]+_}  )) && (( $(echo "${curr_name_max[$name]} < ${prev_name_max[$name]}" |bc -l) ));
         then
-            continue
+            curr_name_max["$name"]=${prev_name_max[$name]}
         else
-            MEMORY=$(jstat -gc $pid | tail -n 1 | awk '{split($0,a," "); sum=a[3]+a[4]+a[6]+a[8]; mb=sum/1024; print mb}')
-            if (( $(echo "${curr_name_max[$name]} < ${prev_name_max[$name]}" |bc -l) ));
-            then
-                curr_name_max["$name"]=${prev_name_max[$name]}
-            fi
-            
-            #output for this pid
-            printf "%-6s %-45s %-7.2f %-7.2f\n" $pid $name $MEMORY ${curr_name_max["$name"]} | sort
+            curr_name_max["$name"]=$MEMORY
         fi
+            
+        #output for current pid
+        printf "%-6s %-45s %-7.2f %-7.2f\n" $pid $name $MEMORY ${curr_name_max["$name"]} | sort
     done
     
     
     
     
     #clean stuff of this iteration
-    unset prev_pid_name
-    declare -A prev_pid_name
     unset prev_name_max
-    declare -A prev_name_max
+    declare -A prev_name_max=()
     
-    #insert all current pid, name and max_memory into prev_associative_array[]
-    for pid in "${!curr_pid_name[@]}";
-    do
-        prev_pid_name[$pid]=${curr_pid_name[$pid]}
-    done
+    #insert all current name and max_memory into prev_associative_array
     for name in "${!curr_name_max[@]}";
     do
         prev_name_max[$name]=${curr_name_max[$name]}
@@ -88,10 +76,8 @@ do
     
     #unset current associative array to make it empty
     unset curr_pid_name
-    declare -A curr_pid_name=([1234]="Jps")
+    declare -A curr_pid_name=()
     unset curr_name_max
-    declare -A curr_name_max=(["Jps"]=0)
-   
-   
-    
+    declare -A curr_name_max=()
+
 done
