0ced83df0fa086e2fd3e9d7caa5330b184bf4dad
amarjeet <a.anand@everteam.com>
2017-09-03 12:24:05 +0530

    working fine with memory output as integers.
    Bugs:
    Enhancement suggested:
    	1. make memory output as float.

--- trackMemory.sh
+++ trackMemory.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
-MEMORY=0.0
+MEMORY=0
 
 declare -A prev_name_max=()
-declare -A curr_pid_name=()
-declare -A curr_name_max=()
 
-echo "pid    Name                                          CurrMem Max "
-echo "====   ============================================  ======= ===="
-printf "\n"
-
-
+echo "pid    Name                                          CurrMem  Max "
+echo "====   ============================================  =======  ===="
 
 
 while true
-do   
+do  
+
+    declare -A curr_pid_name=()
+    declare -A curr_name_max=()
+    
     IFS=$'\n'
     DATA=($("jps"))
     
-    #clear previous loop output from screen
-    tput cuu $(( ${#prev_name_max[@]} )) && tput el
-    
+    #put curser up with # of prev processes
+    if (( ${#prev_name_max[@]} > 0 ));
+    then
+        tput cuu $(( ${#prev_name_max[@]} )) 
+    fi
+
     #for each we get in jps in current loop
     IFS=$' '
     for LINE in "${DATA[@]}"
@@ -41,7 +43,6 @@ do
         else
             curr_name_max["${TOKENS[1]}"]=0
         fi
-        
     done
 
 
@@ -49,8 +50,8 @@ do
     for pid in "${!curr_pid_name[@]}"; 
     do
         name=${curr_pid_name["$pid"]}
-        MEMORY=$(jstat -gc $pid | tail -n 1 | awk '{split($0,a," "); sum=a[3]+a[4]+a[6]+a[8]; mb=sum/1024; print mb}')
-        if (( ${prev_name_max[$name]+_}  )) && (( $(echo "${curr_name_max[$name]} < ${prev_name_max[$name]}" |bc -l) ));
+        MEMORY=$(jstat -gc $pid | tail -n 1 | awk '{split($0,a," "); sum=a[3]+a[4]+a[6]+a[8]; mb=sum/1024; printf "%i", mb}')
+        if [ ${prev_name_max[$name]+_} ] && (( $MEMORY < ${prev_name_max[$name]} ))
         then
             curr_name_max["$name"]=${prev_name_max[$name]}
         else
@@ -58,26 +59,20 @@ do
         fi
             
         #output for current pid
-        printf "%-6s %-45s %-7.2f %-7.2f\n" $pid $name $MEMORY ${curr_name_max["$name"]} | sort
+        printf "%-6s %-45s %-8d %-7d\n" $pid $name $MEMORY ${curr_name_max["$name"]} | sort
     done
     
     
     
     
-    #clean stuff of this iteration
+    #clean stuff of previous iteration
     unset prev_name_max
-    declare -A prev_name_max=()
-    
+    declare -A prev_name_max
+
     #insert all current name and max_memory into prev_associative_array
     for name in "${!curr_name_max[@]}";
     do
         prev_name_max[$name]=${curr_name_max[$name]}
     done
-    
-    #unset current associative array to make it empty
-    unset curr_pid_name
-    declare -A curr_pid_name=()
-    unset curr_name_max
-    declare -A curr_name_max=()
 
 done
