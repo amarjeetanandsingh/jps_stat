811c593ab78e5adcb81ddd734513bb5d00f5ab46
amarjeet <a.anand@everteam.com>
2017-09-03 23:03:09 +0530

    Fixed a max memory bug where one program is started twice
    If a program is started twice, they will have same process_name. So, changed prev_name_max array to prev_pid_max. So that max memory is tracked per pid, not per process_name.

--- jpsstat.sh
+++ jpsstat.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-declare -A prev_name_max=()
+declare -A prev_pid_max=()
+
 echo "/**"
 echo " * PID    : Process Id"
 echo " * Name   : Process Name"
@@ -17,15 +18,15 @@ while true
 do  
 
     declare -A curr_pid_name=()
-    declare -A curr_name_max=()
+    declare -A curr_pid_max=()
     
     IFS=$'\n'
     DATA=($("jps"))
     
     #put curser up with # of prev processes
-    if (( ${#prev_name_max[@]} > 0 ));
+    if (( ${#prev_pid_max[@]} > 0 ));
     then
-        tput cuu $(( ${#prev_name_max[@]} )) 
+        tput cuu $(( ${#prev_pid_max[@]} )) 
     fi
 
     #for each process line we get in jps in current loop
@@ -43,10 +44,10 @@ do
         # insert to associative array
         curr_pid_name["${TOKENS[0]}"]=${TOKENS[1]}
         
-        if [ ${prev_name_max["${TOKENS[1]}"]+_} ]; then
-            curr_name_max["${TOKENS[1]}"]=${prev_name_max["${TOKENS[1]}"]}
+        if [ ${prev_pid_max["${TOKENS[0]}"]+_} ]; then
+            curr_pid_max["${TOKENS[0]}"]=${prev_pid_max["${TOKENS[0]}"]}
         else
-            curr_name_max["${TOKENS[1]}"]=0.0
+            curr_pid_max["${TOKENS[0]}"]=0.0
         fi
     done
 
@@ -56,29 +57,29 @@ do
     do
         name=${curr_pid_name["$pid"]}
         MEMORY=$(jstat -gc $pid | tail -n 1 | awk '{split($0,a," "); sum=a[3]+a[4]+a[6]+a[8]; mb=sum/1024; print mb}')
-        if [ ${prev_name_max[$name]+_} ] && (( $(echo "$MEMORY < ${prev_name_max[$name]}" | bc -l)  ))
+        if [ ${prev_pid_max[$pid]+_} ] && (( $(echo "$MEMORY < ${prev_pid_max[$pid]}" | bc -l)  ))
         then
-            curr_name_max["$name"]=${prev_name_max[$name]}
+            curr_pid_max["$pid"]=${prev_pid_max[$pid]}
         else
-            curr_name_max["$name"]=$MEMORY
+            curr_pid_max["$pid"]=$MEMORY
         fi
             
         #output for current pid
         cpuuse=$(ps -p $pid -o %cpu | tail -n 1 )
-        printf "%-6s %-44s %8.2f %8.2f  %5.1f\n" $pid $name $MEMORY ${curr_name_max["$name"]} $cpuuse | sort
-    done
+        printf "%-6s %-44s %8.2f %8.2f  %5.1f\n" $pid $name $MEMORY ${curr_pid_max["$pid"]} $cpuuse | sort 
+    done 
     
     
     
     
     #clean stuff of previous iteration
-    unset prev_name_max
-    declare -A prev_name_max
+    unset prev_pid_max
+    declare -A prev_pid_max
 
     #insert all current name and max_memory into prev_associative_array
-    for name in "${!curr_name_max[@]}";
+    for pid in "${!curr_pid_max[@]}";
     do
-        prev_name_max[$name]=${curr_name_max[$name]}
+        prev_pid_max[$pid]=${curr_pid_max[$pid]}
     done
 
 done
