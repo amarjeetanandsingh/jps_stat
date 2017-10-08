8ec3a571b545ec0a873dae963c11416b46b8635a
amarjeet <a.anand@everteam.com>
2017-10-08 14:10:47 +0530

    Fixed float number error in output and added RAM use details in output.
    * Fiexd the issue # 1.
    * Added the current ram use and max ram used details in the output

--- README.md
+++ README.md
@@ -1,28 +1,34 @@
 # jps_stat
-###### A bash script to moniter **current_memory** , **max_heap_till_now** , and **current_cpu_by_jvm** details of all java programs running on current linux system.
+
+###### A bash script to moniter **current_heap_memory** , **max_heap_till_now** , **current_ram** , **max_ram_used** , **current_cpu_by_jvm** details of all java programs running on current linux system.
+
 ## How to use
 * Clone this repo and extract and open the folder
 * Right click on ` jpsstat.sh ` file and select ` properties `
 * Under ` Permissions ` tab, check the ` Allow executing file as program ` option.
 * Now open this folder in terminal and enter ` ./jpsstat.sh `
-* This will list the  **current_memory** , **max_heap_till_now** , and **current_cpu_by_jvm** of all running java programs with live refresh.
+* This will list the  **current_heap_memory** , **max_heap_till_now**, **current_ram_use**, **max_ram_used** and **current_cpu_use** of all running java processes on your linux system with live refresh.
+
 ## How to stop
 * Press ` ctrl+c ` to stop the script and enter ` reset ` to clear your screen
+
 ## Script details
 * This uses ` jps ` command to get the list of all processes running java programs (_jvm instances_)
-* Then for each _procecss_id_ obtained from jps, it runs ` jstat -gc <process_id> `
+* Then for each _process_id_ obtained from jps, it runs ` jstat -gc <process_id> `
 * It calculates Heap memory by adding following fields of ` jstat -gc ` output - 
   * ` S0U `: Survivor space 0 utilization
   * ` S1U `: Survivor space 1 utilization
   * ` EU ` : Eden space utilization
   * ` OU ` : Old space utilization
 * It tracks cpu uses using ` ps -p <process_id> -o %cpu ` command 
+* It uses the ` rss ` field of the output of ` /proc/<pid>/statm ` command to get the memory(RAM) used by the java process.
 * It uses ` tput cuu <# of lines> ` to refresh screen
+* All the decimal values in the output is truncated using ` %.* `
+
 ## Requirements
 * Jdk
-* Linux System (Tested on Ubuntu 16.04 LTS)
+* Linux System _(Tested on Ubuntu 16.04 LTS)_
 * Bash version >=  4.0
-
 
 ## License
 MIT License
--- jpsstat.sh
+++ jpsstat.sh
@@ -26,28 +26,32 @@ echo " * PID    : Process Id"
 echo " * Name   : Process Name"
 echo " * CurHeap: Heap memory(MB) currently in use"
 echo " * MaxHeap: Max Heap memory(MB) used by now"
+echo " * CurRAM : Current RAM(MB) used"
+echo " * MaxRAM : Max RAM(MB) used by now"
 echo " * %_CPU  : Current CPU use by PID"
 echo " */"
-echo "=====  ============================================  =======  =======  ====="
-echo " PID                       Name                      CurHeap  MaxHeap  %_CPU"
-echo "=====  ============================================  =======  =======  ====="
+echo "=====  ==============================  =======  =======  ======  ======  ====="
+echo " PID                Name               CurHeap  MaxHeap  CurRAM  MaxRAM  %_CPU"
+echo "=====  ==============================  =======  =======  ======  ======  ====="
 
 
-declare -A prev_pid_max=()
+declare -A prev_pid_max_heap=()
+declare -A prev_pid_max_ram=()
 
 while true
 do  
 
     declare -A curr_pid_name=()
-    declare -A curr_pid_max=()
+    declare -A curr_pid_max_heap=()
+    declare -A curr_pid_max_ram=()
     
     IFS=$'\n'
     DATA=($("jps"))
     
     # put curser up with # of prev processes
-    if (( ${#prev_pid_max[@]} > 0 ));
+    if (( ${#prev_pid_max_heap[@]} > 0 ));
     then
-        tput cuu $(( ${#prev_pid_max[@]} )) 
+        tput cuu $(( ${#prev_pid_max_heap[@]} )) 
     fi
 
     # for each process line we get in jps 
@@ -64,11 +68,19 @@ do
         
         # insert to associative array
         curr_pid_name["${TOKENS[0]}"]=${TOKENS[1]}
-        # get the max memory use from previous loop
-        if [ ${prev_pid_max["${TOKENS[0]}"]+_} ]; then
-            curr_pid_max["${TOKENS[0]}"]=${prev_pid_max["${TOKENS[0]}"]}
+        
+        # get the max heap memory from previous loop
+        if [ ${prev_pid_max_heap["${TOKENS[0]}"]+_} ]; then
+            curr_pid_max_heap["${TOKENS[0]}"]=${prev_pid_max_heap["${TOKENS[0]}"]}
         else
-            curr_pid_max["${TOKENS[0]}"]=0.0
+            curr_pid_max_heap["${TOKENS[0]}"]=0
+        fi
+        
+        # get the max ram used, from previous loop
+        if [ ${prev_pid_max_ram["${TOKENS[0]}"]+_} ]; then
+            curr_pid_max_ram["${TOKENS[0]}"]=${prev_pid_max_ram["${TOKENS[0]}"]}
+        else
+            curr_pid_max_ram["${TOKENS[0]}"]=0
         fi
     done
 
@@ -76,29 +88,47 @@ do
     # get the memroy use for each pid in curr_pid_name
     for pid in "${!curr_pid_name[@]}"; 
     do
-        MEMORY=$(jstat -gc $pid | tail -n 1 | awk '{split($0,a," "); sum=a[3]+a[4]+a[6]+a[8]; mb=sum/1024; print mb}')
-        if [ ${prev_pid_max[$pid]+_} ] && (( $(echo "$MEMORY < ${prev_pid_max[$pid]}" | bc -l)  ))
+        HEAP_MEMORY=$(jstat -gc $pid | tail -n 1 | awk '{split($0,a," "); sum=a[3]+a[4]+a[6]+a[8]; print sum/1024}' )
+        HEAP_MEMORY=${HEAP_MEMORY%.*}
+        RAM_MEMORY=$(( ` cut -d' ' -f2 <<<cat /proc/$pid/statm ` / 1024 ))
+        
+        if [ ${prev_pid_max_heap[$pid]+_} ] && [ $HEAP_MEMORY -lt ${prev_pid_max_heap[$pid]} ]
         then
-            curr_pid_max["$pid"]=${prev_pid_max[$pid]}
+            curr_pid_max_heap["$pid"]=${prev_pid_max_heap[$pid]}
         else
-            curr_pid_max["$pid"]=$MEMORY
+            curr_pid_max_heap["$pid"]=$HEAP_MEMORY
         fi
-            
+        
+        if [ ${prev_pid_max_ram[$pid]+_} ] && [ $RAM_MEMORY -lt ${prev_pid_max_ram[$pid]} ]
+        then
+            curr_pid_max_ram["$pid"]=${prev_pid_max_ram[$pid]}
+        else
+            curr_pid_max_ram["$pid"]=$RAM_MEMORY
+        fi
+          
         #output for current pid
         cpuuse=$(ps -p $pid -o %cpu | tail -n 1 )
-        printf "%-6s %-44s %8.2f %8.2f  %5.1f\n" $pid ${curr_pid_name["$pid"]} $MEMORY ${curr_pid_max["$pid"]} $cpuuse | sort 
+        cpuuse=${cpuuse%.*}
+        printf "%-6s %-30s %8i %8i %7d %7d  %5i\n" $pid ${curr_pid_name["$pid"]:0:30} $HEAP_MEMORY ${curr_pid_max_heap["$pid"]} $RAM_MEMORY ${curr_pid_max_ram["$pid"]} $cpuuse | sort 
     done 
     
     
     # clean stuff of previous iteration
-    unset prev_pid_max
-    declare -A prev_pid_max
-
+    unset prev_pid_max_heap
+    declare -A prev_pid_max_heap
+    unset prev_pid_max_ram
+    declare -A prev_pid_max_ram
 
     # put all current pid and max_memory into prev_associative_array
-    for pid in "${!curr_pid_max[@]}";
+    for pid in "${!curr_pid_max_heap[@]}";
     do
-        prev_pid_max[$pid]=${curr_pid_max[$pid]}
+        prev_pid_max_heap[$pid]=${curr_pid_max_heap[$pid]}
     done
-
+    # put all current pid, max_ram_memory into prev associative array
+    for pid in "${!curr_pid_max_ram[@]}";
+    do
+        prev_pid_max_ram[$pid]=${curr_pid_max_ram[$pid]}
+    done
+    
+    sleep 0.3
 done
